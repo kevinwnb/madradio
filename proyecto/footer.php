1221b3f99d008d014dcfd5551126de1a281d31f0
Kevin Wegener Antolinez <48557454+kevinwnb@users.noreply.github.com>
2021-06-12 13:01:15 -0300

    Update

--- proyecto/footer.php
+++ proyecto/footer.php
@@ -1,4 +1,24 @@
-<div class="container-fluid mt-5">
+<div class="footer">
+    <div class="row text-center mx-auto bg-dark  text-white footer">
+        <div class="col-xs-12 col-sm-12 col-md-4 mb-2 pt-5">
+            <img src="imagenes/Mad radio sin circulo negro.png" class="img-fluid mx-auto" width="25%" ALT="Logo-MadRadio">
+        </div>
+        <div class="col-md-4 pt-4 align-self-center">
+            <p>© 2021 MadRadio - Todos los derechos reservados</p>
+            Politíca de privacidad y condiciones legales
+        </div>
+        <div class="col-md-4 pt-5 align-self-center">
+            <ul class="list-unstyled list-inline social text-center">
+                <li class="list-inline-item"><a href="javascript:void(0)"><i class="fab fa-facebook-square"></i></a></li>
+                <li class="list-inline-item"><a href="javascript:void(0)"><i class="fab fa-twitter"></i></a></li>
+                <li class="list-inline-item"><a href="javascript:void(0)"><i class="fab fa-instagram"></i></a></li>
+                <li class="list-inline-item"><a href="/contacto.php"><i class="fa fa-envelope"></i></a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<div class="d-none container-fluid mt-5">
     <div class="row text-center bg-dark p-5 text-white footer">
         <div class="col-md-4 mb-2 pt-5">
             Politíca de privacidad y condiciones legales
