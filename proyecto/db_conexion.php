9c5764d1bd597a7939d17a4244d3adca382e4635
Kevin Wegener Antolinez <48557454+kevinwnb@users.noreply.github.com>
2021-06-12 17:25:10 -0300

    Update

--- proyecto/db_conexion.php
+++ proyecto/db_conexion.php
@@ -1,5 +1,6 @@
 <?php
-$link = new mysqli('localhost', 'root', '', 'madradio', 3306);
+//$link = new mysqli('localhost', 'root', '', 'madradio', 3306);
+$link = new mysqli('un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com', 'nxwng95hxusgguse', 'euwzk5j82aeayyyq', 'gvyo62mui9wq1klo', 3306);
 
 if ($link->connect_errno) {
     printf("Unable to connect to database: <br> %s", $dbLink->connect_error);
