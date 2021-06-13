0d5b5f989173b2309467ce3a8aa331b42784a6da
Kevin Wegener Antolinez <48557454+kevinwnb@users.noreply.github.com>
2021-06-13 11:24:10 -0300

    Update explorar.php

--- proyecto/explorar.php
+++ proyecto/explorar.php
@@ -65,145 +65,144 @@ $generos = json_decode(file_get_contents($base_url . "/api/generos/all.php"));
                     <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
                 </div>
                 <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="/imagenes/slide1.jpg" class="d-block w-100" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h5>Lo mejor de lo mejor</h5>
-                            <p>Encuentra todo tipo de música y podcasts adaptado a tus intereses.</p>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="/imagenes/slide2.jpg" class="d-block w-100" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h5>Lo mejor de lo mejor</h5>
-                            <p>Encuentra todo tipo de música y podcasts adaptado a tus intereses.</p>
-                        </div>
-                    </div>
-                    <div class="carousel-item">
-                        <img src="/imagenes/slide3.jpg" class="d-block w-100" alt="...">
-                        <div class="carousel-caption d-none d-md-block">
-                            <h5>Lo mejor de lo mejor</h5>
-                            <p>Encuentra todo tipo de música y podcasts adaptado a tus intereses.</p>
-                        </div>
+                    <img src="/imagenes/slide1.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Lo mejor de lo mejor</h5>
+                        <p>Encuentra todo tipo de música y podcasts adaptado a tus intereses.</p>
                     </div>
                 </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
+                <div class="carousel-item">
+                    <img src="/imagenes/slide2.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>MadRadio marcando la diferencia</h5>
+                        <p>Todos vuestros sueños de la musica estan a vuestro alcance aqui.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="/imagenes/slide3.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Pronto Festival MadRadio?</h5>
+                        <p>Apoya este nuevo proyecto para seguir creciendo y poder despegar.</p>
+                    </div>
+                </div>
             </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
 
-            <section id="destacados">
-                <h2 class="text-center pb-3">Destacados</h2>
+        <section id="destacados">
+            <h2 class="text-center pb-3">Destacados</h2>
+            <div class="row g-4 pb-5">
+                <?php
+                $shuffled_pubs = $publicaciones->publicaciones;
+                shuffle($shuffled_pubs);
+                foreach ($shuffled_pubs as $p) {
+                    $usuario = json_decode(file_get_contents($base_url . "/api/usuarios/read.php?id=" . $p->id_usuario));
+                ?>
+                    <div class="col-sm-6 col-md-4 col-xl-3">
+                        <div class="card card-body pub">
+                            <div class="row gx-2">
+                                <div class="col-4">
+                                    <div id="pub-img-wrapper">
+                                        <img src="<?php echo $p->url_imagen ?>">
+                                    </div>
+                                </div>
+                                <div class="col-8">
+                                    <h6><?php foreach ($generos->generos as $g) {
+                                            if ($g->id == $p->id_genero) {
+                                                echo $g->nombre;
+                                                break;
+                                            }
+                                        } ?></h6>
+                                    <p>Subido por <span class="text-warning fw-bold"><?php echo $usuario->nombre ?></span></p>
+                                </div>
+                            </div>
+                            <h5 class="card-title pt-3"><?php echo $p->titulo ?></h5>
+                            <p class="card-text">
+                                <?php echo substr($p->descripcion, 0, 100) . "..." ?>
+                            </p>
+                            <div class="row mx-0 bg-white">
+                                <div class="col-6 text-center px-0">
+                                    <a href="javascript:void(0)" class="d-block btn btn-outline-danger border-2 m-1"><i class="far fa-heart fw-bold"></i></a>
+                                </div>
+                                <div class="col-6 text-center px-0">
+                                    <a id="btn_comment_<?php echo $p->id; ?>" data-bs-toggle="modal" data-bs-target="#exampleModal" href="javascript:void(0)" class="btn-comment d-block btn btn-dark border-2 m-1"><i class="far fa-comment fw-bold"></i></a>
+                                </div>
+                            </div>
+                            <?php $audio_id = uniqid(); ?>
+                            <audio id="<?php echo $audio_id ?>">
+                                <source src="<?php echo $p->url_audio ?>" type="audio/<?php echo substr($p->url_audio, -3) ?>">
+                                Your browser does not support the audio element.
+                            </audio>
+                            <a id="btn-<?php echo $audio_id ?>" onclick="play('<?php echo $audio_id ?>')" href="javascript:void(0)" class="a-reproducir p-3 mt-1 bg-warning text-center">
+                                <i class="fas fa-play"></i><i class="fas fa-stop"></i> <span id="spn-reproducir">Reproducir</span><span id="spn-pausar">Pausar</span>
+                            </a>
+                        </div>
+                    </div>
+                <?php
+                } ?>
+            </div>
+        </section>
+        <?php foreach ($categorias->categorias as $c) { ?>
+            <section id="categoria">
+                <h2 class="text-center pb-3"><?php echo $c->nombre ?></h2>
                 <div class="row g-4 pb-5">
-                    <?php
-                    $shuffled_pubs = $publicaciones->publicaciones;
-                    shuffle($shuffled_pubs);
-                    foreach ($shuffled_pubs as $p) {
+                    <?php foreach ($publicaciones->publicaciones as $p) {
                         $usuario = json_decode(file_get_contents($base_url . "/api/usuarios/read.php?id=" . $p->id_usuario));
+                        if ($p->id_categoria == $c->id) {
                     ?>
-                        <div class="col-sm-6 col-md-4 col-xl-3">
-                            <div class="card card-body pub">
-                                <div class="row gx-2">
-                                    <div class="col-4">
-                                        <div id="pub-img-wrapper">
-                                            <img src="<?php echo $p->url_imagen ?>">
+                            <div class="col-sm-6 col-md-4 col-xl-3">
+                                <div class="card card-body pub">
+                                    <div class="row gx-2">
+                                        <div class="col-4">
+                                            <div id="pub-img-wrapper">
+                                                <img src="<?php echo $p->url_imagen ?>">
+                                            </div>
+                                        </div>
+                                        <div class="col-8">
+                                            <h6><?php foreach ($generos->generos as $g) {
+                                                    if ($g->id == $p->id_genero) {
+                                                        echo $g->nombre;
+                                                        break;
+                                                    }
+                                                } ?></h6>
+                                            <p>Subido por <span class="text-warning fw-bold"><?php echo $usuario->nombre ?></span></p>
                                         </div>
                                     </div>
-                                    <div class="col-8">
-                                        <h6><?php foreach ($generos->generos as $g) {
-                                                if ($g->id == $p->id_genero) {
-                                                    echo $g->nombre;
-                                                    break;
-                                                }
-                                            } ?></h6>
-                                        <p>Subido por <span class="text-warning fw-bold"><?php echo $usuario->nombre ?></span></p>
+                                    <h5 class="card-title pt-3"><?php echo $p->titulo ?></h5>
+                                    <p class="card-text">
+                                        <?php echo substr($p->descripcion, 0, 100) ?>
+                                    </p>
+                                    <div class="row mx-0 bg-white">
+                                        <div class="col-6 text-center px-0">
+                                            <a href="javascript:void(0)" class="d-block btn btn-outline-danger border-2 m-1"><i class="far fa-heart fw-bold"></i></a>
+                                        </div>
+                                        <div class="col-6 text-center px-0">
+                                            <a id="btn_comment_<?php echo $p->id; ?>" data-bs-toggle="modal" data-bs-target="#exampleModal" href="javascript:void(0)" class="btn-comment d-block btn btn-dark border-2 m-1"><i class="far fa-comment fw-bold"></i></a>
+                                        </div>
                                     </div>
+                                    <?php $audio_id = uniqid(); ?>
+                                    <audio id="<?php echo $audio_id ?>">
+                                        <source src="<?php echo $p->url_audio ?>" type="audio/<?php echo substr($p->url_audio, -3) ?>">
+                                        Your browser does not support the audio element.
+                                    </audio>
+                                    <a id="btn-<?php echo $audio_id ?>" onclick="play('<?php echo $audio_id ?>')" href="javascript:void(0)" class="a-reproducir p-3 mt-1 bg-warning text-center">
+                                        <i class="fas fa-play"></i><i class="fas fa-stop"></i> <span id="spn-reproducir">Reproducir</span><span id="spn-pausar">Pausar</span>
+                                    </a>
                                 </div>
-                                <h5 class="card-title pt-3"><?php echo $p->titulo ?></h5>
-                                <p class="card-text">
-                                    <?php echo substr($p->descripcion, 0, 100) . "..." ?>
-                                </p>
-                                <div class="row mx-0 bg-white">
-                                    <div class="col-6 text-center px-0">
-                                        <a href="javascript:void(0)" class="d-block btn btn-outline-danger border-2 m-1"><i class="far fa-heart fw-bold"></i></a>
-                                    </div>
-                                    <div class="col-6 text-center px-0">
-                                        <a id="btn_comment_<?php echo $p->id; ?>" data-bs-toggle="modal" data-bs-target="#exampleModal" href="javascript:void(0)" class="btn-comment d-block btn btn-dark border-2 m-1"><i class="far fa-comment fw-bold"></i></a>
-                                    </div>
-                                </div>
-                                <?php $audio_id = uniqid(); ?>
-                                <audio id="<?php echo $audio_id ?>">
-                                    <source src="<?php echo $p->url_audio ?>" type="audio/<?php echo substr($p->url_audio, -3) ?>">
-                                    Your browser does not support the audio element.
-                                </audio>
-                                <a id="btn-<?php echo $audio_id ?>" onclick="play('<?php echo $audio_id ?>')" href="javascript:void(0)" class="a-reproducir p-3 mt-1 bg-warning text-center">
-                                    <i class="fas fa-play"></i><i class="fas fa-stop"></i> <span id="spn-reproducir">Reproducir</span><span id="spn-pausar">Pausar</span>
-                                </a>
                             </div>
-                        </div>
-                    <?php
+                    <?php }
                     } ?>
                 </div>
             </section>
-            <?php foreach ($categorias->categorias as $c) { ?>
-                <section id="categoria">
-                    <h2 class="text-center pb-3"><?php echo $c->nombre ?></h2>
-                    <div class="row g-4 pb-5">
-                        <?php foreach ($publicaciones->publicaciones as $p) {
-                            $usuario = json_decode(file_get_contents($base_url . "/api/usuarios/read.php?id=" . $p->id_usuario));
-                            if ($p->id_categoria == $c->id) {
-                        ?>
-                                <div class="col-sm-6 col-md-4 col-xl-3">
-                                    <div class="card card-body pub">
-                                        <div class="row gx-2">
-                                            <div class="col-4">
-                                                <div id="pub-img-wrapper">
-                                                    <img src="<?php echo $p->url_imagen ?>">
-                                                </div>
-                                            </div>
-                                            <div class="col-8">
-                                                <h6><?php foreach ($generos->generos as $g) {
-                                                        if ($g->id == $p->id_genero) {
-                                                            echo $g->nombre;
-                                                            break;
-                                                        }
-                                                    } ?></h6>
-                                                <p>Subido por <span class="text-warning fw-bold"><?php echo $usuario->nombre ?></span></p>
-                                            </div>
-                                        </div>
-                                        <h5 class="card-title pt-3"><?php echo $p->titulo ?></h5>
-                                        <p class="card-text">
-                                            <?php echo substr($p->descripcion, 0, 100) ?>
-                                        </p>
-                                        <div class="row mx-0 bg-white">
-                                            <div class="col-6 text-center px-0">
-                                                <a href="javascript:void(0)" class="d-block btn btn-outline-danger border-2 m-1"><i class="far fa-heart fw-bold"></i></a>
-                                            </div>
-                                            <div class="col-6 text-center px-0">
-                                                <a id="btn_comment_<?php echo $p->id; ?>" data-bs-toggle="modal" data-bs-target="#exampleModal" href="javascript:void(0)" class="btn-comment d-block btn btn-dark border-2 m-1"><i class="far fa-comment fw-bold"></i></a>
-                                            </div>
-                                        </div>
-                                        <?php $audio_id = uniqid(); ?>
-                                        <audio id="<?php echo $audio_id ?>">
-                                            <source src="<?php echo $p->url_audio ?>" type="audio/<?php echo substr($p->url_audio, -3) ?>">
-                                            Your browser does not support the audio element.
-                                        </audio>
-                                        <a id="btn-<?php echo $audio_id ?>" onclick="play('<?php echo $audio_id ?>')" href="javascript:void(0)" class="a-reproducir p-3 mt-1 bg-warning text-center">
-                                            <i class="fas fa-play"></i><i class="fas fa-stop"></i> <span id="spn-reproducir">Reproducir</span><span id="spn-pausar">Pausar</span>
-                                        </a>
-                                    </div>
-                                </div>
-                        <?php }
-                        } ?>
-                    </div>
-                </section>
-            <?php } ?>
-        </div>
+        <?php } ?>
+    </div>
     </div>
 
     <?php
