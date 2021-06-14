19ba53bb887779f05349c8fb3570ad0a29e22b26
Nico-GET <78077292+Nico-GET@users.noreply.github.com>
2021-06-14 07:45:28 +0200

    Update explorar.php

--- proyecto/explorar.php
+++ proyecto/explorar.php
@@ -56,7 +56,45 @@ $generos = json_decode(file_get_contents($base_url . "/api/generos/all.php"));
     <script>
         document.querySelector(".navbar li a#explorar").classList.add("active");
     </script>
-   
+   <div id="bg-explorar" class="bg explorar py-5">
+        <div class="container">
+            <div id="carouselExampleCaptions" class="carousel slide mb-5" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    <img src="/imagenes/slide1.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Lo mejor de lo mejor</h5>
+                        <p>Encuentra todo tipo de música y podcasts adaptado a tus intereses.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="/imagenes/slide2.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>MadRadio marcando la diferencia</h5>
+                        <p>Todos vuestros sueños de la musica estan a vuestro alcance en MadRadio.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="/imagenes/slide3.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Pronto Festival MadRadio?</h5>
+                        <p>Apoya este nuevo proyecto para seguir creciendo y poder despegar.</p>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
 
         <section id="destacados">
             <h2 class="text-center pb-3">Destacados</h2>
